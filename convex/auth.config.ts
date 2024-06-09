049094d934298265f83f49230c857165bbd2984a
Saksham292001 <github.com>
2024-06-09 13:18:12 +0530

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://funky-barnacle-67.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default authConfig;
